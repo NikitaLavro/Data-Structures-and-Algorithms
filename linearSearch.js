7fa732d1c2e69c8d3bad4ccf3b35cf1155ae87c0
Nikita <lavro.nikita123@gmail.com>
2023-01-13 17:17:03 -0500

    binarySearch iteration implementation

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,9 @@
-const array = [1, 4, 5, 7, 2, 6, 11, 56, 34, 13];
-let count = 0;
+const linearArray = [1, 4, 5, 7, 2, 6, 11, 56, 34, 13];
+let linearCount = 0;
 
 function linearSearch(array, item) {
   for (let i = 0; i < array.length; i++) {
-    count += 1;
+    linearCount += 1;
     if (array[i] === item) {
       return i;
     }
@@ -11,5 +11,5 @@ function linearSearch(array, item) {
   return null;
 }
 
-console.log(linearSearch(array, 11));
-console.log(`count = ${count}`);
+console.log(linearSearch(linearArray, 11));
+console.log(`count = ${linearCount}`);
