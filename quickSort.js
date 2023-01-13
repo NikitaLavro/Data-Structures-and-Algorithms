aa4589eeab46b467527d24f6a318d09c3c73e0de
Nikita <lavro.nikita123@gmail.com>
2023-01-13 18:24:48 -0500

    quickSort implementation

--- quickSort.js
+++ quickSort.js
@@ -1,4 +1,25 @@
 const sortArray = [0, 3, 5, 6, 3, 2, 4, 8, -1, 7, 25, 32];
 let sortCount = 0;
 
-function quickSort(array) {}
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  let pivotIndex = Math.floor(array.length / 2);
+  let pivot = array[pivotIndex];
+  let less = [];
+  let greater = [];
+  for (let i = 0; i < array.length; i++) {
+    sortCount += 1;
+    if (i === pivotIndex) continue;
+    if (array[i] < pivot) {
+      less.push(array[i]);
+    } else {
+      greater.push(array[i]);
+    }
+  }
+  return [...quickSort(less), pivot, ...quickSort(greater)];
+}
+
+console.log(quickSort(sortArray));
+console.log(sortCount);
