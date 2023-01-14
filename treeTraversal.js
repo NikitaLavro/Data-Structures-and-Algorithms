0670f33f89e1ba0c31f29c988e583c9813249fbd
Nikita <lavro.nikita123@gmail.com>
2023-01-13 19:56:06 -0500

    treeTraversal iteration method

--- treeTraversal.js
+++ treeTraversal.js
@@ -50,4 +50,22 @@ const recursive = (tree) => {
   return sum;
 };
 
+const iteration = (tree) => {
+  if (!tree.length) {
+    return 0;
+  }
+  let sum = 0;
+  let stack = [];
+  tree.forEach((node) => stack.push(node));
+  while (stack.length) {
+    const node = stack.pop();
+    sum += node.v;
+    if (node.c) {
+      node.c.forEach((child) => stack.push(child));
+    }
+  }
+  return sum;
+};
+
 console.log(recursive(tree));
+console.log(iteration(tree));
